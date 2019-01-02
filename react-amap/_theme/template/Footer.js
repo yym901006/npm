1e9d3748bfcea79fd4356a60953e42f284002d11
yangyongming <yangyongming@kuaidihelp.com>
2019-01-02 11:12:02 +0800

    包管理器

--- react-amap/_theme/template/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return <footer>
-    <div className="footer-inner">
-      with ❤ by <a href="https://github.com/ElemeFE/" target="_blank">ElemeFE</a>
-    </div>
-  </footer>;
-}
